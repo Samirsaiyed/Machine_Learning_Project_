a57046b7371f24faf3e6bc367c9329cd9f99374c
Samirsaiyed <samirsaiyed49@gmail.com>
2022-08-18 14:38:15 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 # Declare variables for setup function
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
+VERSION="0.0.3"
 AUTHOR="Samir"
 DESCRIPTION="This is a first FSDS nov batch machine learning project"
-PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 
-    
 def get_requirements_list()->List[str]:
     """
     Description : This function is going to return list of requirements 
     mention in requirements.txt file
 
     return This function is going to return a list which contain name of 
-    libraries mwntioned in requirements.txt file 
+    libraries mentioned in requirements.txt file 
     """
     with open( REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
-
-
+        return requirement_file.readlines().pop("-e .")
 
 
 setup(
@@ -31,9 +27,6 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES,
+packages=find_packages(), #["housing"]
 install_requires=get_requirements_list()
 )
-
-
-#get_requirements_list is woking ro not
