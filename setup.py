0b11e7a2f360f253629c54b37caad9d3594f7691
Samirsaiyed <samirsaiyed49@gmail.com>
2022-08-18 14:54:02 +0530

    uploaded

--- setup.py
+++ setup.py
@@ -1,32 +1,37 @@
-
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 from typing import List
 
-# Declare variables for setup function
-PROJECT_NAME="housing-predictor"
+# Declaring variable for setup function
+PROJECT_NAME="package-Prediction"
 VERSION="0.0.3"
-AUTHOR="Samir"
-DESCRIPTION="This is a first FSDS nov batch machine learning project"
+AUTHOR="Samir Saiyed"
+DESRCIPTION="Machine Learning End to End project for housing data"
+
 REQUIREMENT_FILE_NAME="requirements.txt"
 
+HYPHEN_E_DOT = "-e ."
 
-def get_requirements_list()->List[str]:
+def get_requirements_list() -> List[str]:
     """
-    Description : This function is going to return list of requirements 
+    Description: This function is going to return list of requirement
     mention in requirements.txt file
-
-    return This function is going to return a list which contain name of 
-    libraries mentioned in requirements.txt file 
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
     """
-    with open( REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().pop("-e .")
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
 
 
 setup(
 name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
-description=DESCRIPTION,
-packages=find_packages(), #["housing"]
+description=DESRCIPTION,
+packages=find_packages(), 
 install_requires=get_requirements_list()
 )
